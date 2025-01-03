4ba6ef462b52a61a2bb16e0d9bc202d80d98e3e1
luuming <luuming@hotmail.com>
2025-01-03 17:56:47 +0800

    fix: 修复 esm 引用 json 文件报错的问题

--- test/pick.test.js
+++ test/pick.test.js
@@ -1,22 +1,25 @@
-import pick from "../src/pick.js"
+import { createRequire } from "node:module"
 import assert from "node:assert"
-import origin from "../package.json" assert { type: "json" }
+import pick from "../src/pick.js"
+
+const require = createRequire(import.meta.url)
+const pkg = require("../package.json")
 
 describe("pick up json value", () => {
     it("return the origin with picking up no attributes", () => {
-        const target = pick(origin, { pick: [] })
-        assert.equal(target, origin)
-        assert.equal(target === origin, true)
+        const target = pick(pkg, { pick: [] })
+        assert.equal(target, pkg)
+        assert.equal(target === pkg, true)
     })
     it("without target value", () => {
         const option = [
             ["name"],
             ["version"]
         ]
-        const target = pick(origin, { pick: option })
+        const target = pick(pkg, { pick: option })
         const entries = Object.entries(target)
         for (const [key, value] of entries) {
-            assert.equal(value, origin[key])
+            assert.equal(value, pkg[key])
         }
         assert.equal("author" in target, false)
     })
@@ -25,10 +28,10 @@ describe("pick up json value", () => {
             ["name", "another name"],
             ["version", "another version"]
         ]
-        const target = pick(origin, { pick: option })
+        const target = pick(pkg, { pick: option })
         const entries = Object.entries(target)
         for (const [key, value] of entries) {
-            assert.notEqual(value, origin[key])
+            assert.notEqual(value, pkg[key])
             const index = ["another name", "another version"].indexOf(value)
             assert.equal(index > -1, true)
         }
@@ -38,15 +41,15 @@ describe("pick up json value", () => {
             ["name"],
             ["version"]
         ]
-        const transform = (target, origin) => {
-            target.origin = "origin"
-            target.auther = origin.auther
+        const transform = (target, pkg) => {
+            target.pkg = "pkg"
+            target.auther = pkg.auther
             return target
         }
-        const target = pick(origin, { pick: option, transform })
-        assert.equal(target.name, origin.name)
-        assert.equal(target.version, origin.version)
-        assert.equal(target.origin, "origin")
-        assert.equal(target.auther, origin.auther)
+        const target = pick(pkg, { pick: option, transform })
+        assert.equal(target.name, pkg.name)
+        assert.equal(target.version, pkg.version)
+        assert.equal(target.pkg, "pkg")
+        assert.equal(target.auther, pkg.auther)
     })
 })
